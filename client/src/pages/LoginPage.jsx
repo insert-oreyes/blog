5fb1d1752d835dd650589aae08671c01ac6de1b4
insert-oreyes <oreyes.s@outlook.com>
2024-02-25 20:31:00 +0100

    add logout button and UserContext

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Navigate } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 
 export default function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [redirect, setRedirect] = useState(false)
+  const { setUserInfo } = useContext(UserContext)
 
   async function login(event) {
     event.preventDefault()
@@ -18,7 +20,14 @@ export default function LoginPage() {
       credentials: 'include',
     })
 
-    response.ok ? setRedirect(true) : alert('Wrong credentials')
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo)
+        setRedirect(true)
+      })
+    } else {
+      alert('Wrong credentials')
+    }
   }
   if (redirect) {
     return <Navigate to={'/'} />
