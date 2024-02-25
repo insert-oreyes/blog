16681f6d4baef5430737907314529886e5a3a1b2
Oscar R <99765175+insert-oreyes@users.noreply.github.com>
2024-02-25 20:35:30 +0100

    Logout functionality and context (#6)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useContext } from 'react'
+import { UserContext } from '../UserContext'
 
 /**
  * Renders the header component.
@@ -6,14 +8,47 @@ import { Link } from 'react-router-dom'
  * @returns {JSX.Element} The rendered header component.
  */
 export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext)
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', { credentials: 'include' }).then(
+      (response) => {
+        response.json().then((userInfo) => {
+          setUserInfo(userInfo)
+        })
+      }
+    )
+  }, [])
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    })
+    setUserInfo(null)
+  }
+
+  const username = userInfo?.username
+
   return (
     <header>
       <Link to='/' className='logo'>
         My blog
       </Link>
       <nav>
-        <Link to='/login'>Login</Link>
-        <Link to='/register'>Register</Link>
+        {username && (
+          <>
+            <Link to='/create'>Create new post</Link>
+            <a href='' onClick={logout}>
+              Logout
+            </a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </>
+        )}
       </nav>
     </header>
   )
