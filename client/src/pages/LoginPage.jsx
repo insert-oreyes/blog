a2c074a49eb852c29456f5d6472a039a54ee4477
insert-oreyes <oreyes.s@outlook.com>
2024-02-25 17:08:03 +0100

    add login redirect

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,44 @@
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+
 export default function LoginPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+  async function login(event) {
+    event.preventDefault()
+
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include',
+    })
+
+    response.ok ? setRedirect(true) : alert('Wrong credentials')
+  }
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+
   return (
-    <form className='login'>
+    <form className='login' onSubmit={login}>
       <h1>Login</h1>
-      <input type='text' placeholder='Enter your username' />
-      <input type='password' placeholder='Enter your password' />
+      <input
+        type='text'
+        placeholder='Enter your username'
+        onChange={(event) => setUsername(event.target.value)}
+        value={username}
+      />
+      <input
+        type='password'
+        placeholder='Enter your password'
+        onChange={(event) => setPassword(event.target.value)}
+        value={password}
+      />
       <button type='submit'>Login</button>
     </form>
   )
