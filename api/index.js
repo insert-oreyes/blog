0c19f4aaa8a1c1796fddabbf58c31de1798cbb1e
Oscar R <99765175+insert-oreyes@users.noreply.github.com>
2024-02-26 07:13:54 +0100

    Create new post form  (#7)

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const Post = require('./models/Post')
 const bcrypt = require('bcrypt')
 const app = express()
 const jwt = require('jsonwebtoken')
 const port = 4000
 const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs')
 
 const salt = bcrypt.genSaltSync(10)
 const secret = 'aasdsad'
@@ -61,6 +65,19 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok')
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+  const { originalname, path } = req.file
+  const parts = originalname.split('.')
+  const ext = parts[parts.length - 1]
+  const newPath = path + '.' + ext
+  fs.renameSync(path, newPath)
+
+  const { title, summary, content } = req.body
+  const postDoc = await Post.create({ title, summary, content, cover: newPath })
+
+  res.json(postDoc)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,80 @@
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { useState } from 'react'
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image'],
+    ['clean'],
+  ],
+}
+
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+]
+
+export default function CreatePost() {
+  const [title, setTitle] = useState('')
+  const [summary, setSummary] = useState('')
+  const [content, setContent] = useState('')
+  const [files, setFiles] = useState('')
+
+  async function createNewPost(event) {
+    const data = new FormData()
+    data.set('title', title)
+    data.set('summary', summary)
+    data.set('content', content)
+    data.set('file', files[0])
+    event.preventDefault()
+    console.log(files)
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: data,
+    })
+
+    console.log(await response.json())
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type='title'
+        placeholder={'Title'}
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type='summary'
+        placeholder={'Summary'}
+        value={summary}
+        onChange={(event) => setSummary(event.target.value)}
+      />
+      <input type='file' onChange={(event) => setFiles(event.target.files)} />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button style={{ marginTop: '5px' }}>Create post</button>
+    </form>
+  )
+}
